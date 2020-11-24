fad6eb90678468309e8c1169d51d64e023756ae3
Andras Garzo <garzoand@gmail.com>
2020-11-23 20:51:07 -0800

    small fix

--- test_local_serve.sh
+++ test_local_serve.sh
@@ -1,4 +1,4 @@
 #!/bin/bahs
 container_id=$(docker ps | grep iris-model | cut -f 1 -d ' ')
 docker cp output/* $container_id:/opt/ml/model
-curl -X POST -H 'Content-Type: text/csv' -d @data/smoketest/iris.csv http://localhost:8080/invocations
+curl -X POST -H 'Content-Type: text/csv' --binary-data @data/smoketest/iris.csv http://localhost:8080/invocations
