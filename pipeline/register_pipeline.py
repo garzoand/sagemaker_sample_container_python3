3066f04f134469bc9a9c585f41094fc9726b6a59
Andras Garzo <garzoand@gmail.com>
2021-05-29 11:48:58 -0700

    cm

--- pipeline/register_pipeline.py
+++ pipeline/register_pipeline.py
@@ -21,6 +21,8 @@ from utils import get_pipeline_driver, convert_struct
 
 sys.path.append('../model/')
 
+DEFAULT_REGION='us-west-2'
+
 def main():  # pragma: no cover
     """The main harness that creates or updates and runs the pipeline.
 
@@ -31,68 +33,38 @@ def main():  # pragma: no cover
     )
 
     parser.add_argument(
-        "-n",
-        "--module-name",
-        dest="module_name",
-        type=str,
-        help="The module name of the pipeline to import.",
-    )
-    parser.add_argument(
-        "-kwargs",
-        "--kwargs",
-        dest="kwargs",
-        default=None,
-        help="Dict string of keyword arguments for the pipeline generation (if supported)",
-    )
-    parser.add_argument(
         "-role-arn",
         "--role-arn",
         dest="role_arn",
         type=str,
         help="The role arn for the pipeline service execution role.",
     )
-    parser.add_argument(
-        "-description",
-        "--description",
-        dest="description",
-        type=str,
-        default=None,
-        help="The description of the pipeline.",
-    )
-    parser.add_argument(
-        "-tags",
-        "--tags",
-        dest="tags",
-        default=None,
-        help="""List of dict strings of '[{"Key": "string", "Value": "string"}, ..]'""",
-    )
     args = parser.parse_args()
 
-    if args.module_name is None or args.role_arn is None:
+    if args.role_arn is None:
         parser.print_help()
         sys.exit(2)
-    tags = convert_struct(args.tags)
 
     try:
-        pipeline = get_pipeline_driver(args.module_name, args.kwargs)
+        pipeline = get_pipeline_driver('pipeline', DEFAULT_REGION)
         print("###### Creating/updating a SageMaker Pipeline with the following definition:")
         parsed = json.loads(pipeline.definition())
         print(json.dumps(parsed, indent=2, sort_keys=True))
 
         upsert_response = pipeline.upsert(
-            role_arn=args.role_arn, description=args.description, tags=tags
+            role_arn=args.role_arn, description="Model Pipeline"
         )
         print("\n###### Created/Updated SageMaker Pipeline: Response received:")
         print(upsert_response)
 
-        execution = pipeline.start()
-        print(f"\n###### Execution started with PipelineExecutionArn: {execution.arn}")
+#        execution = pipeline.start()
+#        print(f"\n###### Execution started with PipelineExecutionArn: {execution.arn}")
 
-        print("Waiting for the execution to finish...")
-        execution.wait()
-        print("\n#####Execution completed. Execution step details:")
+        #print("Waiting for the execution to finish...")
+        #execution.wait()
+        #print("\n#####Execution completed. Execution step details:")
 
-        print(execution.list_steps())
+        #print(execution.list_steps())
         # Todo print the status?
     except Exception as e:  # pylint: disable=W0703
         print(f"Exception: {e}")
